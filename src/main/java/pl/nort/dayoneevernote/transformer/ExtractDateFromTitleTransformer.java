d9df56291b108c1fedd9fee4e57feca5b75d7b28
Norbert Potocki <norbert.potocki@gmail.com>
2014-03-15 23:08:46 -0700

    transformer: extrat date from title

---
+++ src/main/java/pl/nort/dayoneevernote/transformer/ExtractDateFromTitleTransformer.java
@@ -0,0 +1,51 @@
+/*
+* Copyright 2014 Norbert Potocki
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package pl.nort.dayoneevernote.transformer;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import pl.nort.dayoneevernote.note.Note;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ *
+ *
+ * @author <a href="mailto:norbert.potocki@gmail.com">Norbert Potocki</a>
+ */
+public class ExtractDateFromTitleTransformer implements NoteTransformer {
+
+    private final String datePattern;
+    private final String titlePattern;
+
+    public ExtractDateFromTitleTransformer(String titlePattern, String datePattern) {
+        this.titlePattern = checkNotNull(titlePattern);
+        this.datePattern = checkNotNull(datePattern);
+    }
+
+    @Override
+    public Note apply(Note note) {
+
+        Note.Builder builder = new Note.Builder().cloneOf(note);
+
+        if(note.getTitle().matches(titlePattern)) {
+            DateTime dt = DateTime.parse(note.getTitle(), DateTimeFormat.forPattern(datePattern).withZoneUTC());
+            builder.withCreationTime(dt);
+        }
+
+        return builder.build();
+    }
+}
---
+++ src/test/java/pl/nort/dayoneevernote/transformer/ExtractDateFromTitleTransformerTest.java
@@ -0,0 +1,84 @@
+/*
+* Copyright 2014 Norbert Potocki
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package pl.nort.dayoneevernote.transformer;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import pl.nort.dayoneevernote.note.Note;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * @author <a href="mailto:norbert.potocki@gmail.com">Norbert Potocki</a>
+ */
+@RunWith(Parameterized.class)
+public class ExtractDateFromTitleTransformerTest extends NoteTransformerTest {
+
+    private static final DateTime DEFAULT_DATE = new DateTime(2014, 3, 15, 22, 0, DateTimeZone.UTC);
+
+    private String titlePattern;
+    private String datePattern;
+    private String title;
+    private DateTime creationDate;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { "", "", "x", DEFAULT_DATE },
+                { "", "yyyy/MM/dd HH:mm", "2014/01/01", DEFAULT_DATE },
+                { "[1-2][0-9]{3}/[01][0-9]/[0-3][0-9]", "yyyy/MM/dd",
+                        "2013/99/31", DEFAULT_DATE },
+                { "[1-2][0-9]{3}/[01][0-9]/[0-3][0-9]", "yyyy/MM/dd",
+                        "2013/12/31", new DateTime(2013, 12, 31, 0, 0, DateTimeZone.UTC)},
+                { "[1-2][0-9]{3}/[01][0-9]/[0-3][0-9] [0-2][0-9]:[0-5][0-9]", "yyyy/MM/dd HH:mm",
+                        "2014/01/01 00:00", new DateTime(2014, 1, 1, 0, 0, DateTimeZone.UTC)},
+                { "[1-2][0-9]{3}/[01][0-9]/[0-3][0-9] [0-2][0-9]:[0-5][0-9]", "yyyy/MM/dd HH:mm",
+                        "2014/01/02 21:12", new DateTime(2014, 1, 2, 21, 12, DateTimeZone.UTC)}
+            }
+        );
+    }
+
+    public ExtractDateFromTitleTransformerTest(String titlePattern, String datePattern, String title, DateTime creationDate) {
+        this.titlePattern = titlePattern;
+        this.datePattern = datePattern;
+        this.title = title;
+        this.creationDate = creationDate;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        transformer = new ExtractDateFromTitleTransformer(titlePattern, datePattern);
+        sourceNote = new Note.Builder()
+                .cloneOf(sourceNote)
+                .withTitle(title)
+                .withCreationTime(DEFAULT_DATE)
+                .build();
+    }
+
+    @Test
+    public void shouldExtractDate() throws Exception {
+        resultNote = transformer.apply(sourceNote);
+
+        assertThat(resultNote.getCreationTime()).isEqualTo(creationDate);
+    }
+}
