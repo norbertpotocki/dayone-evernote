394b0cc139029a6b3a3c5e7230cedaadc3d6b1e5
Norbert Potocki <norbert.potocki@gmail.com>
2014-04-03 01:22:46 -0700

    constant token factory

---
+++ src/main/java/pl/nort/dayoneevernote/evernote/auth/ConstantTokenFactory.java
@@ -0,0 +1,40 @@
+/*
+* Copyright 2014 Norbert Potocki
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package pl.nort.dayoneevernote.evernote.auth;
+
+import org.springframework.stereotype.Component;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * {@link pl.nort.dayoneevernote.evernote.auth.TokenFactory} always returning the same value.
+ *
+ * @author <a href="mailto:norbert.potocki@gmail.com">Norbert Potocki</a>
+ */
+@Component
+public class ConstantTokenFactory implements TokenFactory {
+
+    private final String token;
+
+    public ConstantTokenFactory(String token) {
+        this.token = checkNotNull(token);
+    }
+
+    @Override
+    public String getToken() {
+        return token;
+    }
+}
